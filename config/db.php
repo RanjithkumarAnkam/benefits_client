b5ac2190322ba1a3d0cb5db740335f86e26400f1
Ranjith A <ankamranjithkumar@gmail.com>
2017-06-09 11:15:24 +0530

    updating db details

--- config/db.php
+++ config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=awsdb.cjgq0qzjksrz.us-west-2.rds.amazonaws.com;dbname=sir',
+    'dsn' => 'mysql:host=awsdb.cjgq0qzjksrz.us-west-2.rds.amazonaws.com;dbname=sir_jun8',
 	'tablePrefix' => 'tbl_sir_',
     'username' => 'user',
     'password' => 'Password1!',
