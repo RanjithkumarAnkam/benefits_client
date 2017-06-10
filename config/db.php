c9256b997784cb39233289e17b160bb58026a080
RanjithkumarAnkam <ankamranjithkumar@gmail.com>
2017-06-10 11:08:49 +0530

    Delete db.php

--- config/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=benefits_client',
-    'username' => 'root',
-    'password' => 'Password1!',
-    'charset' => 'utf8',
-];
